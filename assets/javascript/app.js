6ee9f62f9080f5115de10f49225f54f22905f040
rodrigopavezi <rodrigopavezi@gmail.com>
2014-10-16 16:47:50 -0300

    Added routes for app and the texts constants with localization support.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,9 @@
 ---
 ---
 "use strict";
-angular.module("risevision.developer.hub",["ngRoute","risevision.common.header","JSONedit"])
-    .config (["$routeProvider", function ($routeProvider) {
+angular.module("risevision.developer.hub",["ngRoute","risevision.common.header","JSONedit", "pascalprecht.translate", "risevision.common.app"])
+    .config (function ($locationProvider, $routeProvider) {
+
         $routeProvider
             .when("/documentation",
             {
@@ -19,11 +20,43 @@ angular.module("risevision.developer.hub",["ngRoute","risevision.common.header",
                 controller: "DocumentationController",
                 templateUrl: function(params){ return "{{ site.baseurl }}/documentation/search/" +  params.param; }
             })
+            .when("/apps",
+            {
+                controller: "AppsController",
+                templateUrl: "apps/apps.html"
+            })
+            .when("/apps/add",
+            {
+                controller: "AddAppController",
+                templateUrl: "apps/app-form.html"
+            })
+            .when("/apps/edit/:id",
+            {
+                controller: "EditAppController",
+                templateUrl: "apps/app-form.html"
+            })
             .otherwise({redirectTo: "/"});
 
-    }])
+    })
     .config([
         "$interpolateProvider", function($interpolateProvider) {
             return $interpolateProvider.startSymbol("{(").endSymbol(")}");
         }
-    ]);
+    ])
+    .config(['$translateProvider', function ($translateProvider) {
+        $translateProvider.translations('en', {
+            'ADD_APP_TITLE': "Add a New App",
+            'APP_NAME': "App Name",
+            'APP_CLIENT_ID': "Client ID",
+            'SAVE_BUTTON': "Save",
+            'EDIT_BUTTON': "Edit",
+            'APP_DESCRIPTION': "Description",
+            'APP_URL': 'Url',
+            'APP_NO_APP_YET_MESSAGE': "You haven't got an App yet!",
+            'APP_ACTION': 'ACTION',
+            'DELETE_BUTTON': "Delete"
+        });
+
+
+        $translateProvider.preferredLanguage('en');
+    }]);;
