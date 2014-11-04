90c85c899fd5df5ba7401d230de3359e096cc1c4
rodrigopavezi <rodrigopavezi@gmail.com>
2014-11-04 11:39:56 -0200

    Removed apps management from this project just left developer documentation

--- assets/javascript/route.js
+++ assets/javascript/route.js
@@ -5,43 +5,10 @@
  */
 
 "use strict";
-angular.module("risevision.developer.hub")
+angular.module("risevision.documentation")
     .config (["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
 
         $stateProvider
-            .state('apps', {
-                template: "<div ui-view></div>",
-                abstract: true
-            })
-            .state('apps.main', {
-                url: '/apps',
-                templateUrl: 'apps/apps.html',
-                controller: 'MainAppController'
-            })
-            .state('apps.userSignin', {
-                url: '/userSignin',
-                templateUrl: 'apps/apps.userSignin.html',
-                controller: 'MainAppController'
-            })
-            .state('apps.withoutPermission', {
-                url: '/userSignin',
-                templateUrl: 'apps/apps.withoutPermission.html'
-            })
-            .state('apps.list', {
-                url: '/list',
-                templateUrl: 'apps/apps.list.html',
-                controller: 'AppManagementController'
-            })
-            .state('apps.add', {
-                url: '/add',
-                templateUrl: 'apps/app-form.html',
-                controller: 'AddAppController'
-            })
-            .state('apps.edit', {
-                url: '/edit/:id',
-                templateUrl: 'apps/app-edit.html',
-                controller: 'EditAppController'
-            })
             .state('documentation', {
                 url: '/documentation',
                 templateUrl: 'documentation/documentation.html',
