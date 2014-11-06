c89d6bb93196f2abc7a63baf16f7e73a2e4ab62a
rodrigopavezi <rodrigopavezi@gmail.com>
2014-11-06 09:34:44 -0200

    Fixed conflict between angular and liquid templates tags with a different approach so it does not break common-header.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,5 @@
 "use strict";
 angular.module("risevision.documentation",["ui.router","risevision.common.header","JSONedit", "pascalprecht.translate", "ui.bootstrap"])
-
-    .config([
-        "$interpolateProvider", function($interpolateProvider) {
-            return $interpolateProvider.startSymbol("{(").endSymbol(")}");
-        }
-    ])
     .config(['$translateProvider', function ($translateProvider) {
         $translateProvider.translations('en', {
             'DEVELOPER_DOCS': 'Developer Documentation',
